5df48d9ef447954b7d7cfc3fb4bc735f7d31f438
rafi_ahmed <smrafi120@gmail.com>
2024-02-15 16:13:19 +0600

    Fix Unclear Which Hand is Minute and Hour

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ const secondHand = document.querySelector('.second-hand');
 
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg) scale(1)`; // scale(1) is used to reset the scale to 1
 
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    minsHand.style.transform = `rotate(${minsDegrees}deg) scale(0.9)`; // scale(0.9) is used to reduce the size of the hand
 
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg) scale(0.6)`; // scale(0.6) is used to reduce the size of the hand
   }
 
   setInterval(setDate, 1000);
